781c575fbaf4d5578b7c92bab4840b0f31af0001
monasaad <monaaa.saad@yahoo.com>
2024-09-25 11:40:29 +0300

    Initial Commit

---
+++ helloswift/helloswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloswiftApp.swift
+//  helloswift
+//
+//  Created by Mona on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct helloswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ helloswiftTests/helloswiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  helloswiftTests.swift
+//  helloswiftTests
+//
+//  Created by Mona on 22/03/1446 AH.
+//
+
+import Testing
+@testable import helloswift
+
+struct helloswiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
